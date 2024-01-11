c7112331e723bb623118f442a0701861a0f0935f
SANTISOFT <santimasoft@gmail.com>
2024-01-11 17:29:13 +0200

    updting all changes

--- js/script.js
+++ js/script.js
@@ -16,9 +16,9 @@
 
 /*==================== ABOUT TABS ====================*/
 const tabs = document.querySelectorAll('[data-target]'),
-tabContents = document.querySelectorAll('[data-content]');
+    tabContents = document.querySelectorAll('[data-content]');
 tabs.forEach((tab) => {
-    tab.addEventListener('click' , () => {
+    tab.addEventListener('click', () => {
         const target = document.querySelector(tab.dataset.target);
         tabContents.forEach((tabContent) => {
             tabContent.classList.remove('tab__active');
@@ -31,3 +31,40 @@ tabs.forEach((tab) => {
     });
 });
 /*=============== CONTACT FORM =============== */
+const contactForm = document.getElementById('contact-form'),
+    contactName = document.getElementById('contact-name'),
+    contactEmail = document.getElementById('contact-email'),
+    contactSubject = document.getElementById('contact-subject'),
+    contactMessage = document.getElementById('contact-message'),
+    errorMessage = document.getElementById('error-message');
+
+const sendEmail = (e) => {
+    e.preventDefault();
+    //check field if have the values
+    if (contactName.value === '' || contactEmail.value === '' || contactSubject.value === '' || contactMessage.value === '') {
+        //show message
+        errorMessage.textContent = 'Write all the field'
+    }
+    else {
+        //  serviceID - template - #form - publickey
+        emailjs.sendForm('service_r4gp4zh', 'template_91xn2ds', '#contact-form', 'zEPhcisMEtsy2NP4P').then(() => {
+            // show message and ass color, window + dot to open emoji
+            errorMessage.classList.add('color-first');
+            errorMessage.textContent = 'Message sent';
+
+            //remove message after 5 second
+            setTimeout(() => {
+                errorMessage.textContent = '';
+            }, 5000);
+        }, (error) => {
+            alert('OOPS! SOMETHING WENT WRONG', error);
+        });
+        // clear input fields
+        contactName.value = '';
+        contactEmail.value = '';
+        contactSubject.value = '';
+        contactMessage.value = '';
+    }
+};
+;
+contactForm.addEventListener('submit', sendEmail);
