cc899c889004740d2865b9ce3f3853f23c48d364
SANTISOFT <santimasoft@gmail.com>
2024-01-13 12:37:51 +0200

    updting all changes

--- js/script.js
+++ js/script.js
@@ -1,19 +1,49 @@
 /*=============== SHOW MENU ===============*/
-
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close');
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
-
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu');
+        navMenu.style.display = navMenu.classList.contains('show-menu') ? 'block' : 'none';
+    });
+}
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
-
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu');
+    });
+}
 /*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link');
+function linkAction() {
+    const navMenu = document.getElementById('nav-menu');
+    //when we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu');
 
+}
+navLink.forEach((n) => n.addEventListener('click', linkAction));
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
 /*==================== CHANGE BACKGROUND HEADER ====================*/
-
+function scrollHeader() {
+    const header = document.getElementById('header');
+    // when the scroll is greater than 80 viewport height, add the scroll-header class to heeader tag
+    if (this.scrollY >= 80) header.classList.add('scroll-header');
+    else header.classList.remove('scroll-header');
+}
+window.addEventListener('scroll', scrollHeader);
 /*==================== SHOW SCROLL UP ====================*/
-
+function scrollUp() {
+    const scrollUp = document.getElementById('scroll-up');
+    // when the scroll is greater than 350 viewport height, add the show-scroll class to scroll-top
+    if (this.scrollY >= 350) scrollUp.classList.add('show-scroll');
+    else scrollUp.classList.remove('show-scroll');
+}
+window.addEventListener('scroll', scrollUp);
 /*==================== ABOUT TABS ====================*/
 const tabs = document.querySelectorAll('[data-target]'),
     tabContents = document.querySelectorAll('[data-content]');
